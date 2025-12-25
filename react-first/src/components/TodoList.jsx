fde2adfa7db7855efa1e5119b90c5a93cdc0e388
rachaouldbabaali <rachaould@gmail.com>
2025-12-25 19:10:52 +0100

    Add ids and delete todo

--- react-first/src/components/TodoList.jsx
+++ react-first/src/components/TodoList.jsx
@@ -1,31 +1,53 @@
 import { useState } from "react";
 
 function TodoList() {
-    const [input, setInput] = useState("");
-    const [todos, setTodos] = useState([
-        "default todo 1",
-    ]);
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React", completed: false },
+    { id: 2, text: "Learn JavaScript", completed: false },
+  ]);
 
-    const addTodo = () => {
-        if (input.trim() ) {
-            setTodos([...todos, input]);
-            setInput("");
-        }
-        
-    };
+  const addTodo = () => {
+    if (input.trim()) {
+      setTodos([
+        ...todos,
+        { id: todos.length + 1, text: input, completed: false },
+      ]);
+      setInput("");
+    }
+  };
 
-    return <>
-        <h1>Todo List Component</h1>
-        <div>
-            <input type="text" value={input} onChange={e => setInput(e.target.value)} />
-            <button onClick={addTodo}>Add Todo </button>
+  const removeTodo = (id) => {
+    console.log(id);
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
-            <ul>
-                {todos.map((todo , index) => (
-                    <li key={index}>{todo}</li>
-                ))}
-            </ul>
-        </div>
+  const removeAllTodo = () => {
+    setTodos([]);
+  };
+
+  return (
+    <>
+      <h1>Todo List Component</h1>
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button onClick={addTodo}>Add Todo </button>
+        <button onClick={removeAllTodo}>Remove all todos</button>
+
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.text}
+              <button onClick={() => removeTodo(todo.id)}>X</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
+  );
 }
 export default TodoList;
